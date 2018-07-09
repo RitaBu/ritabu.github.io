7f4295fac63df3ee7f6cab4bf5b85b134f30b094
Buritukas <rita.budaviciute@gmail.com>
2018-07-09 22:10:39 +0300

    ivedimo forma in progres

--- makeChart.js
+++ makeChart.js
@@ -3,47 +3,29 @@ var chart;
 var chartConfig = {
   type: "serial",
   dataProvider: [{
-    "year": 2005,
-    "income": 23.5,
-    "expenses": 18.1
+    "kortelesTipas": 'Spalvos',
+    "laikasSekundemis": 11.5
   }, {
-    "year": 2006,
-    "income": 26.2,
-    "expenses": 22.8
+    "kortelesTipas": 'Spalvoti zodziai',
+    "laikasSekundemis": 23.2
   }, {
-    "year": 2007,
-    "income": 30.1,
-    "expenses": 23.9
-  }, {
-    "year": 2008,
-    "income": 29.5,
-    "expenses": 25.1
-  }, {
-    "year": 2009,
-    "income": 24.6,
-    "expenses": 25
+    "kortelesTipas": 'Spalvotos spalvos',
+    "laikasSekundemis": 30.1
   }],
-  categoryField: "year",
+  categoryField: "kortelesTipas",
   categoryAxis: {
     gridPosition: "start"
   },
   valueAxes: [{
-    title: "Million USD"
+    title: "Sekundes"
   }],
   graphs: [{
     type: "column",
-    title: "Income",
-    valueField: "income",
+    title: "Laikas (s) kiekvienai ",
+    valueField: "laikasSekundemis",
     lineAlpha: 0,
     fillAlphas: 0.8,
-    balloonText: "[[title]] in [[category]]:<b>[[value]]</b>"
-  }, {
-    type: "line",
-    title: "Expenses",
-    valueField: "expenses",
-    lineThickness: 2,
-    bullet: "round",
-    balloonText: "[[title]] in [[category]]:<b>[[value]]</b>"
+    balloonText: "Atlikta per:<b>[[value]]</b>s"
   }],
   legend: {
     useGraphSettings: true
