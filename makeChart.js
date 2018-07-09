51b6721cffcf75c2dd4e607571b22d64c470ccfe
Buritukas <rita.budaviciute@gmail.com>
2018-07-09 06:52:18 +0300

    ss
    ss

--- makeChart.js
+++ makeChart.js
@@ -2,11 +2,27 @@ var chart;
 //var data = JSON.parse
 var chartConfig = {
   type: "serial",
-  dataLoader: {
-    url: "duomenys.json",
-    format: "json",
-    "showErrors": true,
-  },
+  dataProvider: [{
+    "year": 2005,
+    "income": 23.5,
+    "expenses": 18.1
+  }, {
+    "year": 2006,
+    "income": 26.2,
+    "expenses": 22.8
+  }, {
+    "year": 2007,
+    "income": 30.1,
+    "expenses": 23.9
+  }, {
+    "year": 2008,
+    "income": 29.5,
+    "expenses": 25.1
+  }, {
+    "year": 2009,
+    "income": 24.6,
+    "expenses": 25
+  }],
   categoryField: "year",
   categoryAxis: {
     gridPosition: "start"
