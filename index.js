697a19f4b6762e8f5399537e465524fc09844f8b
rita.budaviciute@gmail.com <rita.budaviciute@gmail.com>
2022-03-01 11:22:34 +0200

    timer updated

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         myVar = setTimeout(function() {
             document.location.href = 'pabaiga.html';
-        }, 240000);
+        }, 420000);
     };
 
     startTimer = () => {
